0818f83ca90d2158c69cf0403f956f6cd48946ac
vinimedeiros13 <vinimedeiros7@outlook.com>
2022-12-13 07:34:06 -0300

    feat: add project to github

---
+++ src/app.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Notification } from '@prisma/client';
+import { randomUUID } from 'crypto';
+import { CreateNotificationBody } from './create-notification-body';
+import { PrismaService } from './prisma.service';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  async list(): Promise<Notification[]> {
+    return await this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
+  }
+}
